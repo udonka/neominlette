a2f2cefce5ed1eb66ac5dc4abdb4210e4fef19c2
Shuhei Hiya <syuhei176@gmail.com>
2014-09-21 15:53:38 +0900

    add labelstopper.js and add vector.js

---
+++ new/minlette/labelstopper.js
@@ -0,0 +1,22 @@
+(function() {
+	function LabelStopper(ang, str, text) {
+		this.angle = ang;
+		this.labelString = str;
+		this.labelText = text || str;
+	}
+
+	LabelStopper.prototype.getAngle = function() {
+		return this.angle;
+	}
+
+	LabelStopper.prototype.getLabel = function() {
+		return this.labelString;
+	}
+
+	LabelStopper.prototype.getText = function() {
+		return this.labelText;
+	}
+
+}());
+
+
