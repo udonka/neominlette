c3d73a586b3908fbd5d1c7c0f95af40908aebf79
udonka <icymasa@hotmail.co.jp>
2015-01-14 23:09:24 +0900

    almost finished view. I'm going to integrate server and client.

--- new/js/main.js
+++ new/js/main.js
@@ -17,17 +17,14 @@ $(function(){
 	}
 	*/
 
-	var width  = $(window).width();
-	var height = $(window).height();
 
-	var minlength = Math.min(width,height);
-	$("svg").attr("width" , minlength);
-	$("svg").attr("height", minlength);
-
-	$("#container").attr("style", "width:" + width + "px; width:" + height + "px");
-
-	//snapsvgのインスタンス
+	var container = $("#rouletteContainer");
+	var width = container.width();
+	var height = $("html").height() - 200;
+	var length = Math.min(width,height);
 	var snap = Snap("#rouletteArea");
+	snap.dom = document.getElementById("rouletteArea");
+	snap.attr({"style": "width:"+length+"px; height:"+length+"px;"});
 
     var labels = [
         "ごにょごにょ",
@@ -37,13 +34,13 @@ $(function(){
         "宮下",
     ];
 
-	var roulette = new Roulette(snap,labels);
+	var roulette = new Roulette(snap,length,labels);
 	//var rouletteView = new RouletteView(roulette, s, Math.min(width,height) / 2 * 0.8, width/2,height/2);
 
 	var w = 5;
 	var h = 50;
 
-	snap.rect(minlength / 2 - w/2, 0, w, h);
+	snap.rect(length / 2 - w/2, 0, w, h);
 	snap.attr({
 		fill: "#700"
 	})
@@ -61,120 +58,7 @@ $(function(){
 	}, 1000);
 
 
-	var calcSpeed = function(centerX, centerY, startX, startY, endX, endY, timeStampX, timeStampY) {
-		// TODO
-		var vec1 = new Vec2(startX - centerX, startY - centerY);
-		var vec2 = new Vec2(endX - centerX, endY - centerY);
-		var timeDiff = (timeStampY - timeStampX);
-		var theta1 = Angle.createAbsAngle(vec1.getTheta());
-		var theta2 = Angle.createAbsAngle(vec2.getTheta());
-		var dtheta = theta2.calcDiff(theta1);
-
-		if(timeDiff == 0){
-			return 0;	
-
-		}
-
-
-
-
-		var v = (dtheta.get())*1000 / timeDiff;
-
-
-		//!!!!!!!!!!!sendForce(v);
-
-
-		return v;
-	};
-
-	var rouletteStart = function(speed) {
-		console.log("roulette start with speed: " + speed);
-	};
-
-
-	var touchStartX, touchStartY, touchStartTime, touchEndX, touchEndY, touchEndTime;
-
-	var body = document.getElementById("body");
-
-	var arrow = null;
-	body.addEventListener("pointerdown", function(event){
-		touchStartX = event.clientX;
-		touchStartY = event.clientY;
-		touchStartTime =  parseInt((new Date).getTime());
-		// writeMessage('Mousedown or touchstart')	;
-		event.stopPropagation();
-		event.preventDefault();
-
-		arrow =  snap.line(touchStartX,touchStartY,touchStartX,touchStartY)
-			.attr({
-				stroke:Snap.rgb(255,0,0),
-				"stroke-opacity":0.5,
-				strokeWidth:15
-			});
-
-
-	},false);
-
-	body.addEventListener("pointermove", function(event){
-		if(arrow)
-			arrow.attr({
-				x2:event.clientX,
-				y2:event.clientY
-			});
-	});
-
-	//画面外出た時の処理も書かなきゃ
-	body.addEventListener("pointerup", function(event){
-		touchEndX = event.clientX;
-		touchEndY = event.clientY;
-		touchEndTime =  parseInt((new Date).getTime());
-
-
-		rouletteStart(
-			calcSpeed(
-				roulette.x, roulette.y, //しっかり中心を決める必要がある
-				touchStartX, touchStartY,
-				touchEndX, touchEndY,
-				touchStartTime, touchEndTime
-			)
-		);
-
-
-		//s. タッチ場所を描画
-		console.log("pointer up position : " + touchEndX + " "+ touchEndY);
-
-		arrow.attr({
-			x2:touchEndX,
-			y2:touchEndY
-		});
-
-
-		//function にarrowをわたしている クロージャ
-		(function(larrow){
-			arrow.animate({
-				stroke:Snap.rgb(255,255,255), 
-				"stroke-opacity":0
-			},1000,null,function(){
-				//arrowをはずす
-				larrow.remove();
-				//ガベージこれくしょｎ
-				larrow = null;
-			});
-		})(arrow);
-
-		//ガベージこれくしょｎ
-		//moveしても動かないようにする	
-		arrow = null;
-
-
-		//アニメーションで消さなきゃ
-		//arrow = null; //お前にもうようはねえ
-
-		event.stopPropagation();
-		event.preventDefault();
-
-	},false);
-
+	var finger = new RouletteFinger(snap, snap.dom, roulette.x, roulette.y);
 	// layer.add(circle);
 	// layer.add(text);
 	// stage.add(layer);
--- new/js/roulette.js
+++ new/js/roulette.js
@@ -1,11 +1,14 @@
 (function(global){
-	function Roulette(s,labels) {
+	function Roulette(paper,length,labels) {
 		var self = this;
-		self.range = s.width;
 		self.angle = 0;
-		self.v = 0;
+		self.width = length;
+        self.height = length;
+        self.radius = length/2;
+        self.x = self.radius;
+        self.y = self.radius;
 		self.labels = [];
-    	self.group = s.group();
+    	self.group = paper.group();
 
     	var lines = [];
     	var angles = [];
@@ -20,9 +23,7 @@
     	angles.map(function(e, index){
     		//LabelStopperオブジェクトを生成する
     		var label = new LabelStopper(new Angle(e), labels[index], labels[index]+"message");
-
     		self.labels.push(label);
-
     		return label;
 
     	}).map(function(label,index){
@@ -36,21 +37,21 @@
     	}).map(function(label, index) {
     		//対応する図形を描く
     		var ang = label.getAngle().get();
-    		label.path = s.pie(
+    		label.path = paper.pie(
     			0,
     			0,
-    			self.range,
+    			self.radius,
     			ang * 180 / Math.PI,
     			label.next * 180 / Math.PI );
 
-    		var r = self.range ;
+    		var r = self.radius ;
 
     		var diffAng = new Angle(label.next).calcDiff(label.getAngle());
 
     		var middleAng = new Angle(label.getAngle()).add(diffAng.get()/2);
 
 
-    		label.strPath = s.text(0,0, label.getLabel())
+    		label.strPath = paper.text(0,0, label.getLabel())
     			.transform("rotate("+ (-middleAng.get() * 180 / Math.PI) + ") translate("+(r*0.75)+",0) rotate(90)");
 
 			return label;
@@ -77,25 +78,23 @@
     		self.group.append(label.strPath);
     	});
 
-		var circle = self.group.append(s.circle(0,0,self.range/3 *2).attr({fill:Snap.rgb(255,255,255),stroke:Snap.rgb(0,0,0,0)}));
+		var circle = self.group.append(paper.circle(0,0,self.radius/3 *2).attr({fill:Snap.rgb(255,255,255),stroke:Snap.rgb(0,0,0,0)}));
 
-        this.str = s.text(0,0, "asdfasdfasdflakjsdaf");
+        this.str = paper.text(0,0, "asdfasdfasdflakjsdaf");
         console.log(this.str);
         this.str.attr({
             fill : Snap.hsb(0, 1,1), 
             "text-anchor": "middle",
             "font-size":5,
         })
-
-        s.append(this.str);
-
+        paper.append(this.str);
+        this.render();
     }
 
     //requestAnimationFrameにより呼ばれる
     Roulette.prototype.render = function() {
         this.group.transform("translate("+this.x+","+this.y+") rotate("+this.angle/Math.PI*180+")");
         this.str.attr({"text": "hello" + this.angle});
-
     }
 
     //setIntervalとかに呼ばれる？
