aa8a04ee9d265f96e499c8bb79502a5512da61f7
Ubuntu <udonka@minlette-ubuntu.minlette-ubuntu.m1.internal.cloudapp.net>
2015-02-04 13:10:34 +0000

    server can recognize client id by socket.
    server respond to client in console.log.

--- server/models/sio.js
+++ server/models/sio.js
@@ -3,23 +3,30 @@ var Wheel = require('./common/wheel').Wheel;
 
 var wheel = new Wheel(0,0,["aiueo", "kaki", "kukeko", ]);
 //! ラベルの設定はいつかやらなきゃな
-var wheels = {};
+
+var wheels = {};//連想配列のキーを部屋名にして、ホイールのモデルを保存する
 
 
 function sio(server){
   sio = socketio.listen(server);
-  sio.set('transports', [ 'websocket','polling' ]);// いらないっぽい
+  //sio.set('transports', [ 'websocket','polling' ]);// いらないっぽい
 
-  // 接続
+  //あるユーザーが、サーバーに対してコネクションを申し込んできた。 
+  //ここでは、そのユーザー(socket)とのやりとりを定義するのみ。こう言われたらこう返すと
   sio.sockets.on('connection', function(socket){
+    //socket オブジェクトは、サーバーとそのユーザーとのコネクションを表す。
 
+    //クライアントから、初めの挨拶が送られてきたら
     //はじめの初期化
     socket.on('hello', function(data){
-      console.log(data);
+      //クライアントが希望してきた部屋に配属する。
+      //以後、socketはこの部屋に限られる。
+      console.log("hello, "+ socket.id.slice(0,4)+". your room is "+data.room+", OK?");
       socket.join(data.room);
       var wheel = wheels[ data.room+''] = 
         wheels[ data.room+''] || new Wheel(0, 0,["", "", ""]);
 
+      //
       socket.emit('mymove', { 
         f : 0, 
         v : wheel.getVelocity(),
@@ -29,6 +36,7 @@ function sio(server){
 
     // スワイプを受信
     socket.on('swipe', function(data){
+      console.log("hi, <"+socket.id.slice(0,4)+">. good swipe! (force="+data.swipeData.f.toFixed(4)+") in room "+data.room);
       //モデルを取得
       var wheel = wheels[data.room+''] = 
         wheels[data.room+''] || new Wheel(0, 0,["", "", ""]); 
