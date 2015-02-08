c3fae62ae562be75be8fec6fd955053532adcad8
udonka <icymasa@gmail.com>
2015-02-08 22:47:55 +0000

    fully refactored sio.js #41
    introduced Room class which represents the roulette room.
    it has a wheel and a CountDownTimer which is used for gamification.

--- server/models/room.js
+++ server/models/room.js
@@ -1,58 +1,128 @@
+var Wheel = require('./common/wheel').Wheel;
+var CountDownTimer = require('./common/countdowntimer').CountDownTimer;
+
 if(typeof window == "undefined"){
   //require
 }
 
 
-var CountDownTimer = function(count, count_func, finish_func){
+var Room = function(roomid, sio){
+  var this_room = this;
+  this_room.id = roomid;
 
-  //count:number
-  //count_func: function
-  //finish_func: function
-  
-  this.timer = 0;
-  this.count = count;
-  this.count_func = count_func;
-  this.finish_func = finish_func;
+  this_room.sio = sio;
+  this_room.wheel = new Wheel(0,0,["", "", ""]);
+  this_room.countDownTimer = new CountDownTimer();
+  this_room.stopTime = 0;
+
+  this_room.countDownTimer = new CountDownTimer(
+    0,//0まだはじめないのでcountは0にしとく
+    function(count){ //tick
+      if(count <= this_room.stopTime){
+        console.log("STOP TIME!!!!");
+        this_room.wheel.setMovable(false);
+        this_room.sio.in(this_room.id).emit('timer', {count: count , stop:true});
+
+        return ;
+      }
+      this_room.sio.in(this_room.id).emit('timer', {count: count ,stop:false});
+    },
+    function(){ //finished
+      this_room.wheel.setMovable(false);
+    }
+  );
 }
 
-CountDownTimer.prototype.start = function(count){
-  //count:number
-  
-  var this_cdt = this;
+Room.prototype.startCountDown = function(count){
+  var this_room = this;  
 
-  this_cdt.count = count || this_cdt.count;
-
-  if(this_cdt.timer)
-  {
-    console.log("[timer "+this_cdt.timer+"] already running");
-    return;
+  //if started, doesn't restart
+  if(this_room.countDownTimer.working()){
+    console.log("timer is already working");
+    return ;
   }
 
-  this_cdt.timer = setInterval(function(){
-    console.log("[timer "+this_cdt.timer +"] count :" + this_cdt.count);
+  this_room.stopTime = Math.floor(Math.random() * 10);
+  console.log("stopTime " +  this_room.stopTime );
+
+  this_room.countDownTimer.start(count);
+}
 
 
-    if(this_cdt.count <= 0){
-      this_cdt.finish_func();
-      clearInterval(this_cdt.timer);
-      this_cdt.timer = 0;
+Room.prototype.getMembers = function(){
+  
+  var room_members = this.sio.sockets.adapter.rooms[this.id];
+
+  var member_sockets = [];
+  //部屋が存在しなければundefined
+  if(room_members){
+    for(socketid in room_members){
+      member_sockets.push(this.sio.sockets.connected[socketid])
     }
+  }
 
-    this_cdt.count_func(this_cdt.count);
-
-    this_cdt.count--;
-
-  }, 1000);
+  return member_sockets;
 }
 
-CountDownTimer.prototype.working = function(){ 
-  return (!this.timer) ? false:true;
+//部屋のメンバーを見せる
+Room.prototype.showMembers = function(){
+
+  console.log("your room mates [" + this.id + "] are");
+  var members = this.getMembers();
+
+  if(members){
+    for(i in members){
+      console.log("\t" + i + ": <" + 
+          members[i].id.slice(0,4)+">");
+    }
+  }
+  else{
+    console.log("no members in room [" +this.id+ "]");
+  }
 }
 
-CountDownTimer.prototype.stop = function(){ }
+Room.prototype.emitMembers = function(){
 
-var Room = function(){
+  var member_sockets = this.getMembers();
+  var members_array = {};
+
+  for(i in member_sockets){
+    var member_socket = member_sockets[i];
+    members_array[member_socket.client.id] = {
+      hue : id2hue(member_socket.client.id),
+      mp : member_socket.mp
+    }
+  }
+
+  this.sio.in(this.id).emit('members',
+      {members: members_array});
+
+}
+
+Room.prototype.addMp= function(mp){
+      //share mp to room members
+      //
+      //mp should be number;
+      
+      var members_socket = this.getMembers();
+      for(i in members_socket){
+        members_socket[i].mp += mp;
+      }
+};
+
+
+function id2hue(idstr){
+
+  var sum = 0;
+  for(i = 0; i < idstr.length; i++){
+
+    sum += idstr.charCodeAt(i);
+  }
+
+  var hue = sum % 24;
+
+  return hue /24  * 360;
 }
 
 this['Room'] = Room;
-this['CountDownTimer'] = CountDownTimer;
+
--- server/models/sio.js
+++ server/models/sio.js
@@ -1,129 +1,28 @@
 var socketio = require('socket.io');
-var Wheel = require('./common/wheel').Wheel;
+//var Wheel = require('./common/wheel').Wheel;
 var Room = require('./room').Room;
 var CountDownTimer = require('./room').CountDownTimer;
 
 
-Wheel.prototype.startCountDown = function(room, count){
-  var this_wheel = this;  //if started, doesn't restart
-
-  console.log("goodbye");
-  if(!this_wheel.countDownTimer)
-  {
-    this_wheel.stopTime = 0;
-    this_wheel.countDownTimer = new CountDownTimer(
-      0,
-      function(count){
-        if(count <= this_wheel.stopTime){
-          console.log("STOP TIME!!!!");
-          this_wheel.setMovable(false);
-          sio.in(room).emit('timer', {count: count , stop:true});
-
-          return ;
-        }
-        sio.in(room).emit('timer', {count: count ,stop:false});
-      },
-      function(){
-        this_wheel.setMovable(false);
-      }
-    );
-  }
-
-  if(this_wheel.countDownTimer.working()){
-    return ;
-  }
-  console.log("hello");
-
-  this_wheel.stopTime = Math.floor(Math.random() * 10);
-  console.log("stopTime " +  this_wheel.stopTime );
-
-
-  this_wheel.countDownTimer.start(count);
-}
-
 //連想配列のキーを部屋名にして、ホイールのモデルを保存する
-var wheels = {};
+var rooms = {};
 
-
-function sio(server){
-  sio = socketio.listen(server);
+function sio(HTTPserver){
+  sio = socketio.listen(HTTPserver);
+  //sio: Socket.io server.
 
   //あるユーザーが、サーバーに対してコネクションを申し込んできた。 
   //ここでは、そのユーザー(socket)とのやりとりを定義するのみ。こう言われたらこう返すと
   //sio.sockets はデフォルトのNamespace /らしい！
   sio.sockets.on('connection', function(socket){
-    //socket オブジェクトは、サーバーとそのユーザーとのコネクションを表す。
-
-    
-    var getRoomMembers = function(room){
-      var room_members = sio.sockets.adapter.rooms[room];
-
-      var member_sockets = [];
-      //部屋が存在しなければundefined
-      if(room_members){
-        for(socketid in room_members){
-          member_sockets.push(sio.sockets.connected[socketid])
-        }
-      }
-
-      return member_sockets;
-    }
-
-    //部屋のメンバーを見せる
-    var showRoomMembers = function(room){
-      console.log("your room mates [" + room + "] are");
-      var members = getRoomMembers(room);
-
-      if(members){
-        for(i in members){
-            console.log("\t" + i + ": <" + members[i].id.slice(0,4)+">");
-        }
-      }
-      else{
-        console.log("no members in room [" +room+ "]");
-      }
-    }
-
-
-
-    function id2hue(idstr){
-
-      var sum = 0;
-      for(i = 0; i < idstr.length; i++){
-
-        sum += idstr.charCodeAt(i);
-      }
-
-      var hue = sum % 24;
-
-      return hue /24  * 360;
-    }
-
-    //member に変化がおこったときにルームメンバーに送る
-    socket.emitRoomMembers = function(room){
-
-      
-      var member_sockets = getRoomMembers(room);
-      var members_array = {};
-
-      for(i in member_sockets){
-        var member_socket = member_sockets[i];
-        members_array[member_socket.client.id] = {
-          hue : id2hue(member_socket.client.id),
-          mp : member_socket.mp
-        }
-      }
-
-      //socket.broadcast.to(room).emit('members', {members: members_array});
-      sio.in(room).emit('members', {members: members_array});
-
-    }
+    //socket オブジェクトは、サーバーとそのユーザーとのコネクションを表す。つまり、サーバーからしたらユーザーそのもの
 
     //クライアントから、初めの挨拶が送られてきたら
     //はじめの初期化
-    //全然最初に呼ばれるとは限らないクライアントは自信がなくなったらhelloするべき
-    socket.on('hello', function(data){
+    //全然最初に呼ばれるとは限らない
+    //クライアントは自信がなくなったらhelloするべき
 
+    socket.on('hello', function(data){
 
       socket.mp = socket.mp || 0;
 
@@ -133,27 +32,32 @@ function sio(server){
       console.log("hello, <"+ socket.id.slice(0,4)+">. your room is [" + data.room+"], OK?");
       console.log("Here it is your MP! <<" + socket.mp + ">>");
 
-
-      //joinすることにより、to('room')でこの部屋のを受け取れるようになる
+//joinすることにより、to('room')でこの部屋のを受け取れるようになる
       //こんにちは～
+      //!!!!!!!!roomにいれたい
       socket.join(data.room);
 
+
+
+      socket.room = rooms[ data.room+''] = 
+        rooms[ data.room+''] || new Room(data.room, sio); //sio is for broadcast comm
+       
+
       //つまらないものですがポイントどうぞ～
-      addmp(data.room, 5);
+      socket.room.addMp(5); //ここでは送らない
 
-      var wheel = wheels[ data.room+''] = 
-        wheels[ data.room+''] || new Wheel(0, 0,["", "", ""]);
+      socket.room.emitMembers(); //はじめて送信
 
-      showRoomMembers(data.room);
+      socket.room.showMembers();
 
       //現在の状態を渡す
       socket.emit('mymove', { 
         f : 0, 
-        v : wheel.getVelocity(),
-        r : wheel.getAngle().get(), 
+        v : socket.room.wheel.getVelocity(),
+        r : socket.room.wheel.getAngle().get(), 
       });
 
-      socket.emitRoomMembers(data.room);
+      socket.room.emitMembers(data.room);
       
     });
 
@@ -167,41 +71,44 @@ function sio(server){
       }
       else{
         socket.mp --;
-        socket.emitRoomMembers(data.room);
+        socket.emitMembers(data.room);
       }
       */
 
-      
       //joinすることにより、to('room')でこの部屋のを受け取れるようになる
       //helloでやってるけど一応やっとけ
+
       socket.join(data.room);
 
+      socket.room = rooms[ data.room+''] = 
+        rooms[ data.room+''] || new Room(data.room, sio); //sio is for broadcast comm
+       
+
       console.log("");
-      console.log("hi, user <"+socket.id.slice(0,4)+"> in room [" + data.room.slice(0,4) + "]. good swipe! (force="+data.swipeData.f.toFixed(3)+")");
+      console.log( "hi, user <"+ socket.id.slice(0,4)+"> in room [" + socket.room.id
+         + "]. good swipe! (force="+data.swipeData.f.toFixed(3)+")");
 
 
-      showRoomMembers(data.room);
-      socket.emitRoomMembers(data.room);
-      console.log(socket.room);
-      console.log(socket.rooms);
+      socket.room.showMembers();
+      socket.room.emitMembers();
 
 
       //モデルを取得
-      var wheel = wheels[data.room+''] = 
-        wheels[data.room+''] || new Wheel(0, 0,["", "", ""]); 
+
+
 
       //送られてきた力
       var force = data.swipeData.f;
 
       //モデルに対して力を行使
-      wheel.addForce(force);
+      socket.room.wheel.addForce(force);
       //wheel.move();
 
       //送ってきたやつに返す
       
       var f = force;
-      var r = wheel.getAngle().get();
-      var v = wheel.getVelocity() ;
+      var r = socket.room.wheel.getAngle().get();
+      var v = socket.room.wheel.getVelocity() ;
       var swipeData = data.swipeData; 
 
       console.log("("+ f +", "+ r +", "+ v +")");
@@ -223,46 +130,27 @@ function sio(server){
     });
 
 
-    function addmp(room, mp){
-      //share mp to room members
-      //
-      //mp should be number;
-      
-      var members_socket = getRoomMembers(room);
-      for(i in members_socket){
-        members_socket[i].mp += mp;
-      }
-    }
 
     socket.on('addmp', function(data){
-      console.log("ADDMP!! " + data.room);
-      addmp(data.room, 5);
 
-      wheels[data.room].setMovable(true);
+      console.log("ADD MP to everyone in room [" + socket.room.id + "]");
 
-      wheels[data.room].startCountDown(data.room, 15);
+      socket.room.addMp(5);
 
-      socket.emitRoomMembers(data.room);
+      socket.room.wheel.setMovable(true);
+
+      socket.room.startCountDown(15);
+
+      socket.room.emitMembers();
     });
 
 
     // 切断
     socket.on('disconnect', function(){
       console.log("bye bye, <"+socket.id.slice(0,4)+">");
+      //この時点でroomは抜けているが、socket.roomに最後のご挨拶。ぬけまーす
 
-      //この時点でroomは抜けている
-      //
-      //byebyeメッセージが必要かもな
-      /*
-      console.log(socket.rooms);
-      //あばよ
-      for(i in socket.rooms){
-        var roomname = socket.rooms[i];
-        console.log("emit");
-        socket.emitRoomMembers(roomname);
-        socket.leave(roomname);
-      }
-      */
+      socket.room.emitMembers();
 
     });
 
@@ -270,4 +158,3 @@ function sio(server){
 }
 
 module.exports = sio;
-
